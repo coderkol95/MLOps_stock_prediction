6726e65f56408f5c6ee0d94356ca34d6079f0eb0
coderkol95 <innocentangelfromhell@gmail.com>
2023-02-07 08:24:36 +0530

    removing json

--- jobs/download_ticker_data.py
+++ jobs/download_ticker_data.py
@@ -24,7 +24,7 @@ def get_ticker_data(TICKER:str):
         data = {"filename":TICKER,"Date":[1,2,3],"Close":[10,20,30]}
 
         with open(os.environ['GITHUB_OUTPUT'], 'w') as f:
-            f.write(json.dumps(data))
+            f.write(data)
 
         # print(tickerData['Close'])
     except:
